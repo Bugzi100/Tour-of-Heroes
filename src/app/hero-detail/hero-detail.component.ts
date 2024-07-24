d8c5c2ae2d6578d1382454473f52d25957beede6
Marko Baba <markobaba22@gmail.com>
2024-07-24 13:53:42 +0200

    Tour of Heroes steps 5/6

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [FormsModule, NgIf, UpperCasePipe],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
